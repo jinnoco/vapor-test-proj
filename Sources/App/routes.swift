eb73ed2df3c5028d66ace503a48d6b2beec8443c
nofprog <k.jin3217@gmail.com>
2021-10-12 17:43:03 +0900

    アクセスURLの作成

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,6 +7,13 @@ func routes(_ app: Application) throws {
     };app.get { req in
         return "It works!"
     }
+    
+    // http://127.0.0.1:8080/abcで文字列を返す
+    app.get("abc") { req in
+        return "こんにちは"
+    }
+    
+ 
 
     app.get("hello") { req -> String in
         return "Hello, world!"
